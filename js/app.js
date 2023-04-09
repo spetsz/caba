32248157c97526e850db41d9fabd0538f2a7c1ad
spetsz <anewadult@gmail.com>
2023-04-09 01:11:08 +0100

    updated banner section, navigation and changed features icons

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ const dropDownMenu = document.getElementById('navbarSupportedContent')
 
 const bringToView = (e) =>{
 
+    e.parentElement.parentElement.childNodes.forEach(item =>{
+        item.classList = "nav-item"
+    })
+    
+
     dropDownMenu.classList = "collapse navbar-collapse"
     const element = document.getElementById(e.name)
+  
+    e.parentElement.classList.add("active")
     const elementMoreBtn = document.querySelector(`#${e.name} > a`)
     
     element.scrollIntoView()
@@ -21,3 +28,98 @@ const bringToView = (e) =>{
  
 
 }            
+
+const sliderData = [
+    {
+        title : "caba investissement",
+        description : "Invest est l’une des plus proactives des boites de consulting et assistanat en investissement, depuis 2008",
+        src : "./img/new/invest.jpeg",
+        link : "https://www.facebook.com/cabainvest"
+    },
+    {
+        title : "caba immobilier",
+        description : "caba immobilier vous accompagne dans toutes transactions immobilières (Achat – Vente – Location – Echange)",
+        src : "./img/new/imob.jpeg",
+        link : "https://www.facebook.com/profile.php?id=100064823104193"
+    },
+    {
+        title : "caba communication",
+        description :"C’est une agence de communications et de design qui mets en place les moyens de communications pour les entreprises et mêmes les particuliers en offrant les meilleures prestations",
+        src : "./img/new/com2.jpeg",
+        link : "https://www.facebook.com/profile.php?id=100087791501556"
+    },
+]
+
+const title = document.getElementById("slider_title")
+const description = document.getElementById("slider_description")
+const img = document.getElementById("slider_img")
+const anchorTag = document.querySelector(".discover_btn")
+let i = 1
+
+
+const init = () =>{
+    
+    gsap.from("#cont",{
+        opacity : 0,
+        duration : 2,
+        y : -200,
+        ease : "Power.easeInOut"
+    
+    })
+
+    setInterval(()=>{
+
+        if(i < sliderData.length){
+    
+            title.textContent = sliderData[i].title
+            description.textContent = sliderData[i].description
+            img.src = sliderData[i].src
+            anchorTag.href = sliderData[i].link
+
+            i++
+    
+            slide()
+    
+    
+    
+        }else{
+            
+            i = 0
+            title.textContent = sliderData[i].title
+            description.textContent = sliderData[i].description
+            img.src = sliderData[i].src
+            anchorTag.href = sliderData[i].link
+
+            slide()
+            i++
+    
+        }
+    
+    
+    },6000)
+}
+
+const slide = () =>{
+    gsap.from(title,{
+        y : 200,
+        duration : 2,
+        opacity : 0,
+        ease : "Power.easeIn"
+
+    })
+
+    gsap.from(description,{
+        y : -200,
+        duration : 2,
+        opacity : 0,
+        ease : "Power.easeIn"
+
+    })
+
+    gsap.from(img,{
+        opacity : 0,
+        duration : 2
+    })
+}
+
+init()
